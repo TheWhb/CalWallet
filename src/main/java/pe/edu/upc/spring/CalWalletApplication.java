b858a86e609d1518699d08aabe0a23575ef8f84b
Whb <eagreda21@gmail.com>
2021-11-15 18:38:21 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/spring/CalWalletApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalWalletApplication.class, args);
+	}
+
+}
